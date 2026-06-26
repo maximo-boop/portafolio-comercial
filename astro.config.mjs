36219240df9f24a60cb3a2cb0ce28ed291f9a1e4
Maximo <maximo2004lfn@gmail.com>
2026-06-26 20:35:25 -0300

    ui modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: { prefixDefaultLocale: false }
+  },
+
+  vite: { plugins: [tailwindcss()] },
+  integrations: [react()]
 });
